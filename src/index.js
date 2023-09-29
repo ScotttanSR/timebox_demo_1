ca37213331233a8337cbf532b9446fde03d7b09e
Xin Quan Teoh <xqteoh.0611@outlook.com>
2023-09-29 15:14:25 +0800

    Refactor Update Issue logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Resolver from '@forge/resolver';
-import api, { route, storage } from '@forge/api';
+import api, {route, storage} from '@forge/api';
 
 const resolver = new Resolver();
 
@@ -18,22 +18,19 @@ resolver.define('getIssue', async (req) => {
       );
 
     const data = await response.json();
-    console.log("data in get issue", data)
+    console.log('data in get issue', data);
     return data;
   } catch (error) {
     console.log(error);
   }
 });
 
-
 resolver.define('getUser', async (req) => {
-  try{
-    const response = await api
-      .asUser()
-      .requestJira(route`/rest/api/3/myself`, {
-        headers: {
-          'Accept': 'application/json'
-        }
+  try {
+    const response = await api.asUser().requestJira(route`/rest/api/3/myself`, {
+      headers: {
+        Accept: 'application/json',
+      },
     });
     const data = await response.json();
     console.log(`Response: ${response.status} ${response.statusText}`);
@@ -42,21 +39,19 @@ resolver.define('getUser', async (req) => {
   } catch (error) {
     console.log(error);
   }
-})
-
+});
 
 resolver.define('storeData', async (req) => {
-  console.log("data will be store", req.payload);
+  console.log('data will be store', req.payload);
   const {key, data} = req.payload;
 
   await storage.set(key, data);
-
-})
+});
 
 resolver.define('getData', async (req) => {
-  const { key } = req.payload;
+  const {key} = req.payload;
   const storeData = await storage.get(key);
-  console.log("storeData",storeData)
+  console.log('storeData', storeData);
   if (storeData) {
     return storeData;
   }
@@ -66,8 +61,8 @@ resolver.define('getData', async (req) => {
 resolver.define('sendNotification', async (req) => {
   try {
     const issueId = req.context.extension.issue.id;
-    const duration =  req.payload.selectedDuration;
-    console.log("payload:", req.payload.selectedDuration);
+    const duration = req.payload.selectedDuration;
+    console.log('payload:', req.payload.selectedDuration);
 
     var bodyData = `{
       "htmlBody": "Timer started for <strong>${duration}</strong> minutes",
@@ -80,28 +75,28 @@ resolver.define('sendNotification', async (req) => {
 
     const response = await api
       .asUser()
-      .requestJira(route`/rest/api/3/issue/${issueId}/notify`,{
+      .requestJira(route`/rest/api/3/issue/${issueId}/notify`, {
         method: 'POST',
         headers: {
-          'Accept' : 'application/json',
-          'Content-Type': 'application/json'
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
         },
-        body: bodyData
+        body: bodyData,
       });
 
     console.log(`Response: ${response.status} ${response.statusText}`);
-  } catch(error) {
+  } catch (error) {
     console.error('Error sending email notification:', error);
     // Handle the error and return an error message or null if needed
     return null;
   }
-})
+});
 
 resolver.define('sendNotificationFinish', async (req) => {
   try {
     const issueId = req.context.extension.issue.id;
     const issueKey = req.context.extension.issue.key;
-    console.log("payload:", req.payload.selectedDuration);
+    console.log('payload:', req.payload.selectedDuration);
 
     var bodyData = `{
       "htmlBody": "Timer already end for <strong>${issueKey}</strong>",
@@ -114,73 +109,70 @@ resolver.define('sendNotificationFinish', async (req) => {
 
     const response = await api
       .asUser()
-      .requestJira(route`/rest/api/3/issue/${issueId}/notify`,{
+      .requestJira(route`/rest/api/3/issue/${issueId}/notify`, {
         method: 'POST',
         headers: {
-          'Accept' : 'application/json',
-          'Content-Type': 'application/json'
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
         },
-        body: bodyData
+        body: bodyData,
       });
 
     console.log(`Response: ${response.status} ${response.statusText}`);
-  } catch(error) {
+  } catch (error) {
     console.error('Error sending email notification:', error);
     // Handle the error and return an error message or null if needed
     return null;
   }
-})
-
+});
 
 export const handler = resolver.getDefinitions();
 
 export const updateIssue = async (event, context) => {
-  console.log("issue updated to: ", event.issue.fields.status.name);
+  const currentStatus = event.issue.fields.status.name;
   const eventId = event.issue.key;
   const storageData = await getData(eventId);
-  if (storageData !== null && event.issue.fields.status.name !== "To Do"){
-    if (storageData.timeLog.isRunning === false && event.issue.fields.status.name !== "Done") {
-      const newData = {
-        ...storageData,
-        "timeLog": {
-          "isRunning": true,
-          "startTime": new Date().toISOString(), 
-        } 
-      }
-      console.log(newData);
-      storeData(eventId, newData)
-    } else if (storageData.timeLog.isRunning === true && event.issue.fields.status.name === "Done"){
-        const timeLogsHistory = storageData.timeLogsHistory;
-        const now = new Date();
-        const startedDate = new Date(storageData.timeLog.startTime);
-        const elapsedSeconds = Math.floor((now - startedDate)/ 1000);
-        const newEntry = {
-          "startTime": startedDate,
-          "endTime": new Date().toISOString(),
-          "elapsedTime": elapsedSeconds
-        }
-        timeLogsHistory.push(newEntry);
-        const newData = {
-          ...storageData,
-          "timeLog": {
-            "isRunning": false,
-            "startTime": null,
-          },
-          "timeLogsHistory": timeLogsHistory
-        }
-        console.log("newdata to store:",newData);
-        storeData(eventId, newData);
-    } else if (storageData.timeLog.isRunning === true){
-      console.log("Timer is currently running")
-    } else {
-      console.log("Timelog not started");
-    }
-  } else{
-    console.log("Timelog not started at To Do");
+  if (!storageData) {
+    const newTimeLog = {
+      timeLog: {
+        isRunning: currentStatus !== 'To Do' && currentStatus !== 'Done',
+        startTime:
+          currentStatus === 'To Do' || currentStatus === 'Done'
+            ? null
+            : new Date(),
+        status: currentStatus,
+      },
+      timeLogsHistory: [],
+    };
+    storeData(eventId, newTimeLog);
+  } else {
+    const associatedStatuses = event.associatedStatuses;
+    const timeLogsHistory = storageData.timeLogsHistory;
+    const now = new Date();
+    const startedDate = new Date(storageData.timeLog.startTime);
+    const elapsedSeconds = Math.floor((now - startedDate) / 1000);
+    const newEntry = {
+      startTime: startedDate,
+      endTime: new Date().toISOString(),
+      elapsedTime: elapsedSeconds,
+      status: associatedStatuses[0].name,
+    };
+    timeLogsHistory.push(newEntry);
+    const newData = {
+      ...storageData,
+      timeLog: {
+        isRunning: currentStatus !== 'To Do' && currentStatus !== 'Done',
+        startTime:
+          currentStatus === 'To Do' || currentStatus === 'Done'
+            ? null
+            : new Date(),
+        status: currentStatus,
+      },
+      timeLogsHistory: timeLogsHistory,
+    };
+    storeData(eventId, newData);
   }
-
-}
-
+};
 
 const getData = async (key) => {
   const storeData = await storage.get(key);
@@ -190,6 +182,6 @@ const getData = async (key) => {
   return null;
 };
 
-const storeData =  async (key, data) => {
+const storeData = async (key, data) => {
   await storage.set(key, data);
-}
+};
